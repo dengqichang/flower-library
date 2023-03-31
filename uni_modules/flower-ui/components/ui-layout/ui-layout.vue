8354b4beb000d9b4ca369b49cf527539a99b6bd5
dengqichang <deng.qi.chang@qq.com>
2023-03-31 08:55:46 +0800

    布局组件

---
+++ uni_modules/flower-ui/components/ui-layout/ui-layout.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="_ui-layout" :style="getCurrentPageBackground()">
+		<!-- 背景图 -->
+		<image v-if="props.backdrop" class="_ui-layout__backdrop" mode="scaleToFill" :src="props.backdrop" />
+		<!-- 顶栏容器 -->
+		<view class="_ui-layout__header">
+			<!-- 导航容器 -->
+			<slot name="nav" />
+			<!-- 顶部容器 -->
+			<slot name="header" />
+		</view>
+		<!-- 内容容器 -->
+		<view class="_ui-layout__main">
+			<slot />
+		</view>
+		<!-- 自定义标识牌 -->
+		<slot name="signboard" />
+		<!-- 默认标识牌 -->
+		<image v-if="slotSignboard && props.signboard" class="_ui-layout__image" mode="widthFix" :src="props.signboard" />
+		<!-- 弹出层容器 -->
+		<slot name="popup" />
+		<!-- 底部容器 -->
+		<view class="_ui-layout__footer">
+			<slot name="footer" />
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {useSlots} from "vue";
+	import {getIsCustomNav,getCurrentPageBackground} from "@/uni_modules/flower-api";
+	import {layout} from "@/uni_modules/flower-ui/core/attribute.js"
+	// 判断子党员标识牌是否存在内容
+	const slotSignboard = !useSlots().signboard;
+	// 处理H5端兼容nav、header的slot内容高度
+	const isNavTop = getIsCustomNav() ? '44px' : '0px';
+	// 属性
+	const props = defineProps({
+		backdrop: {type: String,default: layout.backdrop},
+		signboard: {type: String,default: layout.signboard}
+	})
+</script>
+
+<style scoped>
+	._ui-layout {min-height: 100vh;display: flex;flex-direction: column;}
+	._ui-layout__backdrop {position: fixed;width: 100vw;height: 100vh;}
+	._ui-layout__main {flex: 1;z-index: 1;}
+	._ui-layout__image {width: 100vw;}
+	._ui-layout__footer {position: sticky;bottom: 0;z-index: 2;}
+	._ui-layout__header {
+		position: sticky;z-index: 2;
+		/* #ifndef H5 */
+		top: 0px;
+		/* #endif */
+		/* #ifdef H5 */
+		top: v-bind(isNavTop);
+		/* #endif */
+	}
+</style>
