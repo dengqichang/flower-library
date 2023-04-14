4832fb8ee7ff580068691f85d44c7975d49be949
dengqichang <deng.qi.chang@qq.com>
2023-04-14 11:21:52 +0800

    自定义

--- uni_modules/flower-empty/components/ui-custom-empty/ui-custom-empty.vue
+++ uni_modules/flower-empty/components/ui-custom-empty/ui-custom-empty.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="_ui-empty">
+	<view class="_ui-empty" :style="styles">
 		<view class="_ui-empty__svg" :style="{'backgroundImage':'url('+ svg +')'}" />
 		<view class="_ui-empty__description">{{props.description}}</view>
 		<slot />
@@ -8,19 +8,28 @@
 
 <script setup>
 	import {svgToUrl} from "../../core";
-	import {getColors,defaultColors} from "@/uni_modules/flower-config";
-	import {watch,ref} from "vue";
+	import {getColors,defaultColors,unitConversion} from "@/uni_modules/flower-config";
+	import {watch,ref,computed} from "vue";
 	const svg = ref("");
 	const props = defineProps({
 		svg: {type: String,default: ""},
 		description: {type: String,default: ""},
 		primaryColor: {type: String,default: ""},
 		clothingColor: {type: String,default: ""},
-		otherColor: {type: String,default: ""}
+		otherColor: {type: String,default: ""},
+		offsetTop: {type: [String,Number],default: 0},
+		offsetBottom: {type: [String,Number],default: 0}
 	});
 	watch(() => defaultColors, () => {
 		svg.value = svgToUrl(props.svg, getColors(props.primaryColor),  getColors(props.clothingColor), getColors(props.otherColor));
 	}, {deep: true,immediate: true});
+	
+	const styles = computed(()=>{
+		return {
+			marginTop:unitConversion(props.offsetTop),
+			marginBottom:unitConversion(props.offsetBottom)
+		}
+	});
 </script>
 
 <style scoped>
