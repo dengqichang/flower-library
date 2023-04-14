d610ecd443575348df98edfa045d0ac624392e50
dengqichang <deng.qi.chang@qq.com>
2023-04-14 11:29:54 +0800

    colors

--- uni_modules/flower-config/core/colors.js
+++ uni_modules/flower-config/core/colors.js
@@ -1,15 +1,28 @@
-import {reactive} from "vue";
-import {generate} from '../plug-in/design-colors';
-import {defaultConfig,defaultPrimary} from "./config.js";
+import {
+	reactive
+} from "vue";
+import {
+	generate
+} from '../plug-in/design-colors';
+import {
+	defaultConfig,
+	defaultPrimary
+} from "./config.js";
 
 // 颜色解析器
 const colorGenerate = (e) => {
-	const temp = generate(defaultConfig.colors[e]);for (let t in temp) {colors[`${e}-${Number(t)+1}`] = temp[t];};colors[e] = temp[5];
+	const temp = generate(defaultConfig.colors[e]);
+	for (let t in temp) {
+		colors[`${e}-${Number(t)+1}`] = temp[t];
+	};
+	colors[e] = temp[5];
 };
 // 初始化全局颜色
 let colors = reactive({});
-for (let i in defaultConfig.colors) {colorGenerate(i);};
-colors = Object.assign(colors,defaultConfig.presets);
+for (let i in defaultConfig.colors) {
+	colorGenerate(i);
+};
+colors = Object.assign(colors, defaultConfig.presets);
 /**
  * 设置主题
  */
@@ -29,17 +42,25 @@ const restoreTheme = () => {
 /**
  * 参数是否为内置变量，如果不是则直接返回
  */
-const isBuiltColor = (c) => {return colors[c] ? colors[c] : c};
+const isBuiltColor = (c) => {
+	return colors[c] ? colors[c] : c
+};
 /**
  * 获取内置颜色
  */
-const getColors = (c) => {return isBuiltColor(c)};
+const getColors = (c) => {
+	return isBuiltColor(c)
+};
 
 /**
- * 单位转化
+ * 单位转化，number时为rpx，string自带单位
  */
 const unitConversion = (e) => {
-	if (typeof e == "number") {return `${e}rpx`} else if (typeof e == "string") {if (e.indexOf('px') != -1) {return `${e}`} else {return `${e}rpx`};};
+	if (typeof e == "number") {
+		return `${e}rpx`
+	} else if (typeof e == "string") {
+		return `${e}`
+	};
 };
 
 export {
