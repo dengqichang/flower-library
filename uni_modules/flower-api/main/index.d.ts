a715a7dc723ac0bbd546e9fb96fa1fff0ac54c33
dengqichang <deng.qi.chang@qq.com>
2023-04-19 12:04:14 +0800

    api

--- uni_modules/flower-api/main/index.d.ts
+++ uni_modules/flower-api/main/index.d.ts
@@ -1,10 +1,4 @@
-// pages.js
-declare function getCurrentPageRoute() : void;
-declare function getCurrentPageTitle() : void;
-declare function getIsCustomNav() : void;
-declare const globalStyle : object;
 
 export {
-	// pages.js
-	getCurrentPageRoute,getCurrentPageTitle,getIsCustomNav,globalStyle,
+	
 }
