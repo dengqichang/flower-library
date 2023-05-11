6e2d761ddf1d3da45872428b4ed1188d5864ad9c
dengqichang <deng.qi.chang@qq.com>
2023-05-12 00:12:51 +0800

    化妆美妆图标组件

---
+++ uni_modules/flower-icons/components/icon-jewelry/icon-jewelry.vue
@@ -0,0 +1,19 @@
+<template><text :class="['flower-icons',customClass]" :style="[iconStyle]">{{showIcon}}</text></template>
+<script setup>
+	// #ifdef APP-NVUE
+	import iconUrl from './iconfont.ttf';const domModule = weex.requireModule('dom');domModule.addRule('fontFace', {'fontFamily': "flower-icon-jewelry",'src': "url('" + iconUrl + "')"});
+	// #endif
+	import {unitConversion,getColors} from "@/uni_modules/flower-config";
+	import {computed} from "vue";
+	const props = defineProps({isFill: Boolean,color: String,isBold: Boolean,size: [String, Number],lineHeight: [String, Number],customStyle: Object,customClass: Object});
+	const iconStyle = computed(() => {let style = {...props.customStyle};if (!!props.size) {style.fontSize = unitConversion(props.size);};if (!!props.color) {style.color = getColors(props.color);};if (props.isBold) {style.fontWeight = 'bold';};if (!!props.lineHeight) {style.lineHeight = unitConversion(props.lineHeight);};return style;});
+	const showIcon = computed(() => {if (props.isFill) {return '\ue71f'} else {return '\ue6ee'};});
+</script>
+<style scoped>
+	/* #ifndef APP-NVUE */
+	@font-face {font-family: flower-icons;src: url('iconfont.ttf') format('truetype');}.flower-icons {font-family: flower-icons;font-size: 28rpx;}
+	/* #endif */
+	/* #ifdef APP-NVUE */
+	.flower-icons {font-family: flower-icon-jewelry;font-size: 28rpx;}
+	/* #endif */
+</style>
